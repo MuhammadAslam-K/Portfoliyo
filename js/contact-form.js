35aca02555a5aec2697a2ab4018ac6f6653f12e4
MuhammadAslam-K <aslamka.2k3@gmail.com>
2023-04-13 01:11:21 +0530

    finished wanted to test responsive & links

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,46 +1,7 @@
-// function submitForm() {
-//     var name = $('input[type="text"][placeholder="Your Name"]').val();
-//     var email = $('input[type="text"][placeholder="Your Email"]').val();
-//     var subject = $('input[type="text"][placeholder="Subject"]').val();
-//     var message = $('textarea[placeholder="Message"]').val();
-    
-//     // Validate the input fields
-//     if (name == '' || email == '' || subject == '' || message == '') {
-//       alert('Please fill in all the fields.');
-//       return;
-//     }
-  
-//     // Submit the data to Google Sheets
-//     var params = {
-//       'name': name,
-//       'email': email,
-//       'subject': subject,
-//       'message': message
-//     };
-//     $.ajax({
-//       url: 'https://script.google.com/macros/s/AKfycbwrtAFfWonmyVkmZJyF5pXlRxqC0tUQ96UsLF8Ei8x9iW69xvth6vvZJ-4U2pPe9SZZ/exec',
-//       method: 'GET',
-//       dataType: 'json',
-//       data: params,
-//       success: function(response) {
-//         alert('Your message has been sent!');
-//         $('#submit-form')[0].reset();
-//       },
-//       error: function(response) {
-//         alert('There was an error sending your message. Please try again later.');
-//       }
-//     });
-//   }
-// $('#submit-form').submit(function(event) {
-//     event.preventDefault();
-//     submitForm();
-//   });
-
-
 
 function Validation(){
   var name = document.getElementById('name').value
-  var phone = document.getElementById('phone').value
+  // var phone = document.getElementById('phone').value
   var email = document.getElementById('email').value
   var message = document.getElementById('message').value
 
